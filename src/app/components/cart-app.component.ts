0a054656a5f6f435362d584da1cf27120a6a1b7c
Ignacio <104097123+ignaciogiacotto@users.noreply.github.com>
2024-05-30 17:02:19 -0300

    creando y configurando componentes cart, catalog y product cart y creando models produc, y cartItem

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../models/product';
+import { CatalogComponent } from './catalog/catalog.component';
+import { CartComponent } from './cart/cart.component';
+import { CartItem } from '../models/cartItem';
 
 @Component({
   selector: 'cart-app',
   standalone: true,
-  imports: [],
+  imports: [CatalogComponent, CartComponent],
   templateUrl: './cart-app.component.html'
 })
-export class CartAppComponent {
+export class CartAppComponent implements OnInit{
+  
+  products: Product[] = [];
+
+  items: CartItem[] = [];
+
+  constructor(private service: ProductService){
+  }
+
+  ngOnInit(): void {
+    this.products = this.service.findAll();
+  }
+
+  onAddCart(product: Product){
+    this.items = [... this.items, {product:{... product}, quantity: 1}];
+  }
 
 }
---
+++ src/app/data/product.data.ts
@@ -0,0 +1,40 @@
+import { Product } from "../models/product";
+
+export const products: Product[] = [
+    {
+        id: 1,
+        name: 'Teclado Asus RGB',
+        description: 'Este es un teclado asus mecanico rgb',
+        price: 1000
+    },
+    {
+        id: 2,
+        name: 'Samsung Smart TV LED 75"',
+        description: 'Un excelente tv OLED',
+        price: 1800
+    },
+    {
+        id: 3,
+        name: 'Sony Camara Video',
+        description: 'Camara de video para Streaming',
+        price: 3000
+    },
+    {
+        id: 4,
+        name: 'Memoria RAM Corsair 16GB DDR5',
+        description: 'memoria RAM optimizada para videojuegos',
+        price: 2700
+    },
+    {
+        id: 5,
+        name: 'NVIDIA Asus RTX4090 16GB',
+        description: 'Tarjeta de video NVIDIA optimizada para multitareas',
+        price: 6200
+    },
+    {
+        id: 6,
+        name: 'CPU Intel Core I7',
+        description: 'CPU optimizada para multitarea',
+        price: 5000
+    }
+]
