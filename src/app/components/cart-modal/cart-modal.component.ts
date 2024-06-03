57161b632cf085a4f70f871201f33ec5e766d30a
Ignacio <104097123+ignaciogiacotto@users.noreply.github.com>
2024-06-03 10:47:02 -0300

    Creando componente Cart-modal y haciendo el carrito modal

--- src/app/components/cart-modal/cart-modal.component.ts
+++ src/app/components/cart-modal/cart-modal.component.ts
@@ -1,13 +1,14 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CartComponent } from '../cart/cart.component';
 import { CartItem } from '../../models/cartItem';
 
 @Component({
-  selector: 'cart',
+  selector: 'cart-modal',
   standalone: true,
-  imports: [],
-  templateUrl: './cart.component.html'
+  imports: [CartComponent],
+  templateUrl: './cart-modal.component.html'
 })
-export class CartComponent {
+export class CartModalComponent {
 
   @Input() items: CartItem[] = [];
   @Input() total = 0;
@@ -16,6 +17,12 @@ export class CartComponent {
   
   onDeleteCart(id: number){
     this.idProductEventEmitter.emit(id);
+  }
+
+  @Output() closeEventEmitter = new EventEmitter();
+
+  closeCart(): void{
+    this.closeEventEmitter.emit();
   }
 
 }
