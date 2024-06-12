c17823bebd3de7a6029d4558620ed0721ddafed6
Ignacio <104097123+ignaciogiacotto@users.noreply.github.com>
2024-06-12 11:09:43 -0300

    Agregando al proyecto cart-app el framework Redux. Creando folder store y archivos actions y reducer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { itemsReducer } from './store/items.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideStore(
+    {
+      items: itemsReducer,
+      
+    }
+  )]
 };
--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -31,29 +31,14 @@ export class CartAppComponent implements OnInit{
   ngOnInit(): void {
     this.products = this.service.findAll();
     this.items = JSON.parse(sessionStorage.getItem('cart') || '[]');
-    this.calculateTotal();
+    /* this.calculateTotal(); */
     this.onDeleteCart();
     this.onAddCart();
   }
 
   onAddCart(): void{
     this.sharingDataService.productEventEmitter.subscribe(product =>{
-      const hasItem = this.items.find(item => item.product.id === product.id);
-      if(hasItem) {
-        this.items = this.items.map(item => {
-          if(item.product.id === product.id){
-            return {
-              ... item,
-              quantity: item.quantity + 1
-            }
-          }
-          return item;
-        })
-      }else{
-        this.items = [... this.items, {product:{... product}, quantity: 1}];
-  
-      }
-      this.calculateTotal();
+      /* this.calculateTotal(); */
       this.saveSession();
       this.router.navigate(['/cart'], {
       state: {items: this.items, total: this.total}
@@ -80,12 +65,8 @@ export class CartAppComponent implements OnInit{
         confirmButtonText: "Si, eliminar."
       }).then((result) => {
         if (result.isConfirmed) {
-          this.items = this.items.filter(item => item.product.id !== id);
-          if(this.items.length == 0){
-            sessionStorage.removeItem('cart');
-            sessionStorage.clear();
-          }
-          this.calculateTotal();
+
+          /* this.calculateTotal(); */
           this.saveSession();    
           this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
             this.router.navigate(['/cart'], {
@@ -100,10 +81,6 @@ export class CartAppComponent implements OnInit{
         }
       });
     })
-  }
-
-  calculateTotal(): void {
-    this.total = this.items.reduce( (accumulator, item) => accumulator + item.quantity * item.product.price, 0);
   }
 
   saveSession(): void{
